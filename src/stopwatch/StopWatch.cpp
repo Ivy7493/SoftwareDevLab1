a56643ff7cd0d65b3671d4b4c8e6a9f0a148d5f4
Ivan Von Staden <ivanblizz23@gmail.com>
2021-08-10 11:43:12 +0200

    Exercise 5.1

--- src/stopwatch/StopWatch.cpp
+++ src/stopwatch/StopWatch.cpp
@@ -1,10 +1,41 @@
 #include "StopWatch.h"
 #include <ctime>
+#include <iostream>
 using namespace std;
 
 // returns the amount of time in seconds that has passed since the process (i.e. your program) started executing 
-double getProcessTime()
+
+
+double Watch::getProcessTime()
 {
 	clock_t time = clock();
-	return static_cast<double>(time)/CLOCKS_PER_SEC;
+	return static_cast<double>(time) / CLOCKS_PER_SEC;
+}
+
+bool Watch::startTimer()
+{
+	if (!this->TimerState) {
+		this->TimerState = true;
+		this->StartTime = this->getProcessTime();
+		return true;
+	}
+	else if (this->TimerState) {
+		return false;
+	}
+	return false;
+}
+
+double Watch::endTimer()
+{
+	if (this->TimerState) {
+		this->TimerState = false;
+		this->EndTime = this->getProcessTime();
+		this->DeltaTime = (this->EndTime - this->StartTime);
+		return this->DeltaTime;
+	}
+	else if (!this->TimerState) {
+		cout << "Please Start Timer before you can stop it" << endl;
+		return -1;
+	}
+	return 0.0;
 }
--- src/stopwatch/StopWatch.h
+++ src/stopwatch/StopWatch.h
@@ -2,7 +2,19 @@
 #define STOPWATCH_H
 
 // returns the amount of time in seconds that has passed since the process (i.e. your program) started executing 
-double getProcessTime();
+class Watch {
+public:
+	double getProcessTime();
+	bool startTimer();
+	double endTimer();
+private:
+	bool TimerState = false;
+	double StartTime;
+	double EndTime;
+	double DeltaTime;
+};
+
+
 
 
 #endif
